02891b90dbb13ebea9c7da6df45e1db646f7783b
refu15 <unicon.versity.nect@gmail.com>
2025-10-21 18:58:30 +0900

    revert(frontend): vite config alias and api import path to original alias-based config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@/lib': path.resolve(__dirname, './src/lib'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
